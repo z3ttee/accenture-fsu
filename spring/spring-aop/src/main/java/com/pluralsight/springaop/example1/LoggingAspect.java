089e9e679f6e83251d596dfa7c897d916e9db4e9
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-04-07 10:51:49 +0200

    Initial commit of angular

---
+++ spring/spring-aop/src/main/java/com/pluralsight/springaop/example1/LoggingAspect.java
@@ -0,0 +1,17 @@
+package com.pluralsight.springaop.example1;
+
+import java.util.logging.Logger;
+
+public class LoggingAspect {
+
+    private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
+
+    public void before() {
+        logger.info("Entering method");
+    }
+
+    public void after() {
+        logger.info("Exiting method");
+    }
+
+}
--- spring/spring-security-conference/src/main/java/com/pluralsight/conference/controller/RegistrationController.java
+++ spring/spring-security-conference/src/main/java/com/pluralsight/conference/controller/RegistrationController.java
@@ -10,10 +10,27 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.text.NumberFormat;
+import java.util.Calendar;
 
 @Controller
 public class RegistrationController {
 
+    @GetMapping("registration")
+    public String getRegistration(@ModelAttribute("registration") Registration registration) {
+        return "registration";
+    }
 
+    @PostMapping("registration")
+    @Secured("ROLE_USER")
+    public String addRegistration(@Valid @ModelAttribute("registration") Registration registration, BindingResult result) {
+        if(result.hasErrors()) {
+            System.out.println("There were errors");
+            return "registration";
+        }
+
+        System.out.println("Registration: " + registration.getName());
+        return "redirect:registration";
+    }
 
 }
