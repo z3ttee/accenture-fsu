dcf63d5f61381a82006d8a757c690de1b7d843b5
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-04-08 13:42:04 +0200

    get data via http

--- angular/acme-product-management-angular-demo/src/app/products/product-list.component.ts
+++ angular/acme-product-management-angular-demo/src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 import { IProduct } from "./product";
 import { ProductService } from "./product.service";
 
@@ -7,12 +8,14 @@ import { ProductService } from "./product.service";
     templateUrl: "./product-list.component.html",
     styleUrls: ["./product-list.component.css"]
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
     
     pageTitle: string = "Product List";
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string = "";
+    sub!: Subscription;
 
     products: IProduct[] = [];
     filteredProducts: IProduct[] = [];
@@ -39,8 +42,17 @@ export class ProductListComponent implements OnInit {
     }
 
     public ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
+        this.sub = this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error: err => this.errorMessage = err
+        });
+    } 
+
+    public ngOnDestroy(): void {
+        this.sub.unsubscribe();
     }
 
     onRatingClicked(message: string): void {
