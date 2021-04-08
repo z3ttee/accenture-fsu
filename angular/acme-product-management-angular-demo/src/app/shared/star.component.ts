31c237527a3a61949614ea776ae3861373e3879e
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-04-08 09:43:29 +0200

    implemented rating as nested component

---
+++ angular/acme-product-management-angular-demo/src/app/shared/star.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
+
+@Component({
+    selector: "pm-star",
+    templateUrl: "./star.component.html",
+    styleUrls: [ "./star.component.css" ]
+})
+export class StarComponent implements OnChanges {
+    @Input() rating: number = 0;
+    cropWidth: number = 75;
+
+    ngOnChanges(): void {
+        this.cropWidth = this.rating * 75/5;
+    }
+}
