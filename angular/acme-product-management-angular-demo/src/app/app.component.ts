34895843a98da9dd74c11ceb5f324928e39b54f3
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-04-08 08:15:58 +0200

    outsourced component's template

--- angular/acme-product-management-angular-demo/src/app/app.component.ts
+++ angular/acme-product-management-angular-demo/src/app/app.component.ts
@@ -2,12 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'pm-root',
-  template: `
-    <div>
-      <h1>{{ pageTitle }}</h1>
-      <pm-products></pm-products>
-    </div>`
+  templateUrl: "./app.component.html"
 })
 export class AppComponent {
-  pageTitle: string = "Das ist ein Titel";
+  pageTitle: string = "Acme Product Management";
 }
