a5f2a8a6ca1561dd589d2d3ccd9b11a1910da2c2
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-04-09 10:27:37 +0200

    routing via code

--- angular/acme-product-management-angular-demo/src/app/products/product-detail.component.ts
+++ angular/acme-product-management-angular-demo/src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from './product';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -7,11 +8,27 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = "Product Detail";
+  product?: IProduct;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) { }
 
   public ngOnInit(): void {
-    const id = this.activatedRoute.snapshot.paramMap.get("id")
+    const id = Number(this.activatedRoute.snapshot.paramMap.get("id"))
+
+    this.product = {
+      "productId": id,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2021",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "assets/images/leaf_rake.png"
+    }
+  }
+
+  public onBack(): void {
+    this.router.navigate(["/products"])
   }
 
 }
