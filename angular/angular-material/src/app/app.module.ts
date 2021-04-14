3dcefa6c8b09066fb17833bc9735835c69902b1c
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-04-14 09:15:40 +0200

    added avatars

--- angular/angular-material/src/app/app.module.ts
+++ angular/angular-material/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
   { path: "", loadChildren: () => import("./contactmanager/contactmanager.module").then(m => m.ContactmanagerModule)},
@@ -18,6 +19,7 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     RouterModule.forRoot(routes),
   ],
   providers: [],
