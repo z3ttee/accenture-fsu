708a5a873a5ebab0bb2f16765240acecea8b3ca8
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-04-20 11:57:50 +0200

    catch and replace error handling

--- angular/angular-rxjs/src/app/products/product-list.component.ts
+++ angular/angular-rxjs/src/app/products/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Observable } from 'rxjs';
+import { EMPTY, Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
@@ -19,7 +20,12 @@ export class ProductListComponent implements OnInit {
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.products$ = this.productService.getProducts()
+    this.products$ = this.productService.getProducts().pipe(
+      catchError((err) => {
+        this.errorMessage = err
+        return EMPTY; // or use: of([])
+      })
+    );
   }
 
   onAdd(): void {
